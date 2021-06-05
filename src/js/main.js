c7fc766ab419c264f939722fed1195c62f0343ff
uzinok <uzinok@yandex.ru>
2021-06-05 16:27:29 +0300

    regexp

--- src/js/main.js
+++ src/js/main.js
@@ -23,7 +23,7 @@ class Coverage {
 
             ranges.push(rangeUp);
 
-            let regexp = /[}|\s|]}/g;
+            let regexp = /[}|\f\n\r\t\v​\u00A0\u1680​\u180e\u2000​\u2001\u2002​\u2003\u2004​\u2005\u2006​\u2007\u2008​\u2009\u200a​\u2028\u2029​\u2028\u2029​\u202f\u205f​\u3000|]}/g;
             regexp.lastIndex = pos;
 
             let posIndex = regexp.exec(text);
